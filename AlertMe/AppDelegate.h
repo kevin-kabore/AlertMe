317558b5fc5d167f655a5a51ba9664902b61dbfa
Kevin Kabore <kevinkabore@172-17-50-78.dynapool.nyu.edu>
2016-03-11 15:25:24 -0500

    Initial Commit

---
+++ AlertMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertMe
+//
+//  Created by Kevin Kabore on 3/11/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertMe
+//
+//  Created by Kevin Kabore on 3/11/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
